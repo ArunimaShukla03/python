0baf90ca7a0903c2bf11c587203e7bbc30a9f328
ArunimaShukla03 <arunimashukla2003@gmail.com>
2022-09-16 02:12:45 +0530

    added files

---
+++ if_statements.py
@@ -0,0 +1,34 @@
+is_male = False
+
+#does not print the statement if its false otherwise prints the statement
+
+if is_male:
+  print("you are a male")
+else:
+  print("you are not a male")
+
+# we may also use "or" keyword if either of the statements is true, the statement below it gets executed
+
+is_female = True
+is_tall = True
+
+if is_female or is_tall:
+  print("you are a female or tall or both")
+else:
+  print("you are neither female or tall")
+
+  # we may use "and" keyword if both of the statements are true, the statement below it gets executed
+
+  # elif is else-if statement
+
+is_gay = True
+is_happy = False
+
+if is_gay and is_happy:
+  print("I am happy for you.")
+elif is_gay and not(is_happy):
+  print("You have internalized homophobia.")
+elif not(is_gay) and is_happy:
+  print("You are homophobic.")
+else:
+  print("I am sorry")
---
+++ return.py
@@ -0,0 +1,8 @@
+# parameter is used to give information to a function and return is used to get a value from a function.
+
+# no code is executed in a block after the return statement
+
+def cube(num):
+  print(num*num*num)
+
+result = cube(4)
