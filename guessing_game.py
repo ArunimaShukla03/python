eca437b6abb177248ca4d8706c508d9bf001a4b7
ArunimaShukla03 <arunimashukla2003@gmail.com>
2022-09-18 18:24:47 +0530

    guessing game

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,20 @@
+from re import T
+
+
 secret_word = "sickness"
 guess = " "
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
 
-while guess!=secret_word:
-  guess = input("Guess the word: ")
+while guess!=secret_word and not(out_of_guesses):
+  if guess_count < guess_limit:
+   guess = input("Guess the word: ")
+   guess_count += 1
+  else:
+    out_of_guesses = True
 
-print("You win.")
+if out_of_guesses:
+  print("You are out of guesses, you lose :(")
+else:
+  print("You win!")
