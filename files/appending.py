af493fb4b8ebd4f0d93919f52b303774895f35f0
ArunimaShukla03 <arunimashukla2003@gmail.com>
2022-09-19 02:47:17 +0530

    added files

---
+++ files/appending.py
@@ -0,0 +1,7 @@
+employee_file = open("files/employees.txt", "a")
+
+employee_file.write("toby - human resources")
+
+employee_file.write("\nkelly-customer service")
+
+employee_file.close()
---
+++ files/writing.py
@@ -0,0 +1,3 @@
+employee_file = open("files/employees1.txt", "w")
+
+employee_file.write("new file")
