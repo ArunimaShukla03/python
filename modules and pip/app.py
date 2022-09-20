b216f2e9ce5ee829c6046fd1dbec8e498a472cf7
ArunimaShukla03 <arunimashukla2003@gmail.com>
2022-09-20 22:56:02 +0530

    added file

---
+++ modules and pip/app.py
@@ -0,0 +1,11 @@
+import useful_tools
+
+#this imports all the functions from the usedful_tools file
+
+import calendar
+
+#this is a external module in python for a user to utilize
+
+print(calendar.month(2020, 9))
+
+print(useful_tools.roll_dice(10))
