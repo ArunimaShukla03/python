23e875fd82dc3fd7d433b574db088a5907b13c8b
ArunimaShukla03 <arunimashukla2003@gmail.com>
2022-09-23 23:58:16 +0530

    objects

---
+++ classes and objects/app.py
@@ -0,0 +1,7 @@
+from student import Student
+
+#importing "Student" class from "student" file
+
+student1 = Student("Jim", "business", 3.2, False)
+
+print(student1.name)
---
+++ classes and objects/student.py
@@ -0,0 +1,11 @@
+#classes and objects are used to create your own datatype
+
+#classes basically define what the datatype is while the object is actually the datatype
+
+class Student:
+
+  def _init_(self, name, major, gpa, is_on_probation):
+    self.name = name
+    self.major = major
+    self.gpa = gpa
+    self.is_on_probation = is_on_probation
