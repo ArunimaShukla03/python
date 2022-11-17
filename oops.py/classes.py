adf07f92b439356b80986af0edb1056f5b45fcfa
ArunimaShukla03 <arunimashukla2003@gmail.com>
2022-11-17 23:55:19 +0530

    classes

--- oops.py/classes.py
+++ oops.py/classes.py
@@ -1,6 +1,19 @@
 #OOPs is Object Oriented Programming
 
-x=1
-print(type("Hello"))
+class Person ():
+  pass
 
-#
+# In order to avoid the IDE from stating that there is nothing inside the class, we use "pass" keyword.
+
+# Now to create an object of class "Person"
+
+person1 = Person()
+
+# Now to assign some attributes to class
+
+person1.age = 24
+person1.name = "Arunima"
+person1.favourite_color = "none"
+
+print(person1.age)
+print(person1.name)
