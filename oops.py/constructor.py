35a28dfef49ee1ef7fb4223bdf23af2bb92d7fe7
ArunimaShukla03 <arunimashukla2003@gmail.com>
2022-11-19 18:26:28 +0530

    init

--- oops.py/constructor.py
+++ oops.py/constructor.py
@@ -1,7 +1,25 @@
 # The __init__ function runs as soon as the instance of a class is created.
 
-class Employee():
+class Creation():
   def __init__(self):
     print("OH IM CREATED")
     
-employee1 = Employee()
+create1 = Creation()
+
+class Employee():
+  def __init__(self, name, role):
+    self.name = name
+    self.role = role
+    
+employee1 = Employee(name='arunima', role='developer')
+print(employee1.name)
+
+class Book():
+  def __init__(self, name, PageCount):
+    self.name = name
+    self.PageCount = PageCount
+    
+read = Book(name = "five feet apart", PageCount="350")
+
+print("I finished the book, it had", read.PageCount , "pages.")
+
